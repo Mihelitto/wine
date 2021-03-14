ed188e1f902ea90d50ebda24f2bb2c3a533e1f0e
Michael Yudin <mihelitto@gmail.com>
2021-03-14 17:49:24 +0300

    add years variable to template

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+from datetime import date
 
+env = Environment(
+    loader=FileSystemLoader('.'),
+    autoescape=select_autoescape(['html', 'xml'])
+)
+
+template = env.get_template('template.html')
+years = date.today().year - 1920
+
+render_page = template.render(years = years)
+
+with open("index.html", "w", encoding="utf8") as f:
+    f.write(render_page)
 
 server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
 server.serve_forever()
