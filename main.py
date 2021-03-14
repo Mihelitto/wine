97c6170fe4531edd5f36ec84dc8315cda7f91163
Michael Yudin <mihelitto@gmail.com>
2021-03-14 18:47:49 +0300

    wine cards is filled out from excel file

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from datetime import date
+import pandas
+
+wine_df = pandas.read_excel("wine.xlsx")
+wines = wine_df.to_dict(orient="Records")
 
 env = Environment(
     loader=FileSystemLoader('.'),
@@ -10,7 +14,7 @@ env = Environment(
 template = env.get_template('template.html')
 years = date.today().year - 1920
 
-render_page = template.render(years = years)
+render_page = template.render(years = years, wines=wines)
 
 with open("index.html", "w", encoding="utf8") as f:
     f.write(render_page)
